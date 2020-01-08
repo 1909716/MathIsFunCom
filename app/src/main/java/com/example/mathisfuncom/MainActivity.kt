382e29b5a37c9ddd8ea8b7e79c538a8bb24eeaa7
handsomejy <1909716@users.noreply.github.com>
2020-01-08 21:40:09 +0800

    joeying commit at 09:39pm 8/1/2020

--- app/src/main/java/com/example/mathisfuncom/MainActivity.kt
+++ app/src/main/java/com/example/mathisfuncom/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mathisfuncom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnStart = findViewById(R.id.startButton) as Button
+        val btnQuit = findViewById(R.id.quitButton) as Button
+
+        btnStart.setOnClickListener{
+            setContentView(R.layout.menu)
+        }
+
+        btnQuit.setOnClickListener{
+            System.exit(-1)
+        }
+
     }
+
 }
